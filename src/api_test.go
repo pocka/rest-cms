ca26c676718bdff5324fd1d04a0dc155b1eede9a
pocka <pocka.moe@gmail.com>
2017-05-21 00:27:11 +0900

    Add API test

---
+++ src/api_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/appleboy/gofight"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPing(t *testing.T) {
+	gofight.New().
+	GET("/ping").
+	Run(NewServer(), func(res gofight.HTTPResponse, req gofight.HTTPRequest) {
+		assert.Equal(t, "pong!", res.Body.String())
+		assert.Equal(t, 200, res.Code)
+	})
+}
